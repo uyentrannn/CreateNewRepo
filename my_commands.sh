aad9e1069311281d408bc7ef4e95c96ab7c72a76
Uyen <uyen.trannn09@gmail.com>
2020-08-18 01:08:05 +1000

    updated bash script

--- my_commands.sh
+++ my_commands.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 function create() {
-    python create.py
-    echo $1
+    cd
+    python3 create.py $1
+    cd /Users/uyentran/Desktop/Admin/MyProjects/$1
+    git init
+    git remote add origin https://github.com/uyentrannn/$1.git
+    touch README.md
+    git add .
+    git commit -m "Inital commit"
+    git push -u origin master
+    code .
 }
